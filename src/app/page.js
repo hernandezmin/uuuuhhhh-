5986f3288e154637ce66832aa75f847d3c2eccb6
Naomi Jimena Minagi Hernández <nj.minagi@ccb.edu.gt>
2024-08-28 10:49:20 -0600

    Add info section

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 
-export default function Home() {
- return (
-  <main>
-    <header>
-      <div>
-        <h1>
-        Hello
-        </h1>
-      </div>
-    </header>
-  </main>
- ) 
-}
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <header className={styles.header}>
         <div>
           <h1>
@@ -31,10 +16,37 @@ export default function Home() {
               <span>I am</span>
             </span>
             <br />
-            <span>Josh Carlson</span>
+            <span>Naomi Minagi</span>
           </h1>
         </div>
       </header>
+      <section className={styles.infoSection}>
+        <img
+        src="/imagen-para-website.png"
+        alt="Imagen"
+        className={styles.image}
+      />
+      <div className={styles.infoContainer}>
+        <span className={styles.title + " " + styles.tellowText}>Naomi</span> 
+        <br />
+        <span className={styles.title}>Minagi</span>
+      </div>
+      <div className={styles.list}>
+        <ul>
+          <li>
+            <span className={styles.grayText}>Age: </span> 16
+            </li>
+          <li>
+            <span className={styles.grayText}>Nationality:</span>Guatemalteca
+            </li>
+          <li> 
+          <span className={styles.grayText}>Skill set:</span>Bach en compu
+          </li>
+          <li> 
+          <span className={styles.grayText}>Languages:</span> English, Spa</li>
+        </ul>
+      </div>
+      </section>
     </main>
   );
 }
